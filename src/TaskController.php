7f10fb9c035a647aada314b171b188945134a658
Rasim Aghayev <rasimaqayev@gmail.com>
2021-12-26 01:23:22 +0400

    Step 7 REST-API DB connect

--- src/TaskController.php
+++ src/TaskController.php
@@ -17,9 +17,14 @@ class TaskController
                 $this->respondMethodNotAllowed("GET,POST");
             }
         }else{
+            $task=$this->gateway->get($id);
+            if ($task===false){
+                $this->respondNotFound($id);
+                return;
+            }
             switch ($method){
                 case "GET":
-                    echo "show $id";
+                    echo json_encode($task);
                     break;
                 case "PATCH":
                     echo "update $id";
@@ -38,5 +43,11 @@ class TaskController
         http_response_code(405);
         header("Allow: $allowed_methods");
 
+    }
+
+    private function respondNotFound(string $id):void
+    {
+        http_response_code(404);
+        echo json_encode(["message"=>"Task with ID $id not found"]);
     }
 }
--- src/TaskGateway.php
+++ src/TaskGateway.php
@@ -12,6 +12,26 @@ class TaskGateway
     public function getAll(): array
     {
         $sql="SELECT * FROM `tasks` order by 'name'";
-        return $this->conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        $stmt=$this->conn->query($sql);
+//        return $this->conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        $data=[];
+        while ($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+            $row['is_completed']= (bool) $row['is_completed'];
+            $data[]=$row;
+        }
+        return $data;
+    }
+
+    public function get(string $id): array | false
+    {
+        $sql="SELECT * FROM `tasks` where id=:id";
+        $stmt=$this->conn->prepare($sql);
+        $stmt->bindValue(":id",$id,PDO::PARAM_INT);
+        $stmt->execute();
+        $data=$stmt->fetch(PDO::FETCH_ASSOC);
+        if ($data!==false){
+            $data['is_completed']=(bool) $data['is_completed'];
+        }
+        return $data;
     }
 }
