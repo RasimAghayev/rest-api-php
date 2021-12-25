a044bd2cddfb3a1335f29a90d2a30cfd6c3cecc0
Rasim Aghayev <rasimaqayev@gmail.com>
2021-12-25 15:20:12 +0400

    Step 2 REST-API

--- api/index.php
+++ api/index.php
@@ -1,2 +1,14 @@
 <?php
-echo "hello world";
+$path=parse_url($_SERVER["REQUEST_URI"],PHP_URL_PATH);
+$parts=explode("/",$path);
+$resource=$parts[2];
+$id=$parts[3] ?? null;
+echo $resource ,", ", $id;
+echo $_SERVER["REQUEST_METHOD"],"\n";
+if($resource !== "tasks"){
+    var_dump($resource);
+//    header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
+    http_response_code(404);
+    exit;
+
+}
